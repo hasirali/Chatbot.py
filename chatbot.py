18773b6f559a188e4ee2ed8564ecea57a21d1b34
Hasir <hasira804@gmail.com>
2026-04-23 00:39:05 +0530

    added chat history

--- chatbot.py
+++ chatbot.py
@@ -5,9 +5,13 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 
+chat_history = []
+
 while True:
     user_input = input('You: ')
     if user_input == 'exit':
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history + [user_input])
+    chat_history.append(user_input)
+    chat_history.append(result.content)
     print("Bot: ",  result.content)
